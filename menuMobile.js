f6bd50a68f26fbe9dccdf46c0fa01483b0a59640
Carlos Bekinho <carlos.dev.job@gmail.com>
2023-08-15 23:42:45 -0300

    dropdown mobile update

--- menuMobile.js
+++ menuMobile.js
@@ -1,52 +1,73 @@
 
-    const btnMobile = document.querySelector('.btn-mobile-menu')
-    const headerList = document.querySelector(".header-list")
-    const dropBtn = document.querySelector('.drop-btn')
-    const dropdownContent = document.querySelector('.dropdown-content')
-    const headerListItens = document.querySelector(".header-list-itens")
-    
+const btnMobile = document.querySelector('.btn-mobile-menu')
+const headerList = document.querySelector(".header-list")
+const dropBtn = document.querySelector('.drop-btn')
+const dropdownContent = document.querySelector('.dropdown-content')
+const headerListItens = document.querySelector(".header-list-itens")
+const voltarBtn = document.querySelector('.item-title-container')
 
-    function verificarTamanhoDaTela() {
-  if (window.innerWidth <= 921) {
-    btnMobile.addEventListener('click', openMobileMenu)
-    
-    
-    
-    function openMobileMenu() {
+
+
+function verificarTamanhoDaTela() {
+    if (window.innerWidth <= 921) {
+        btnMobile.addEventListener('click', openMobileMenu)
+
+
+
+        function openMobileMenu() {
+
+            btnMobile.classList.toggle('active')
+            headerList.classList.toggle('open-mobile-menu')
+
+        }
+
+        dropBtn.addEventListener('click', openDropMenu)
+
+
+
+        function openDropMenu() {
+
+
+            dropdownContent.style.display = 'flex'
+
+        }
+
+        voltarBtn.addEventListener('click', itenBtnVoltar)
+
+        function itenBtnVoltar() {
+
+
+            dropdownContent.style.display = 'none'
+
+        }
         
-        btnMobile.classList.toggle('active')
-        headerList.classList.add('open-mobile-menu')
+        function fecharMenu(){
+            dropdownContent.style.display = 'none'
+
+        }
+
+        btnMobile.addEventListener('click', verificarClasse);
+
+        function verificarClasse() {
+            if (btnMobile.classList.contains('active')) {
+                fecharMenu();
+                
+            }
+        }
+
+        
+
+    }else{
+        dropdownContent.style.display = ''
         
     }
-    
-    dropBtn.addEventListener('click', openDropMenu)
-    
-    
-
-    function openDropMenu() {
-
-        headerList.removeChild(headerListItens)
-        dropdownContent.style.display = 'flex'
-
-    }
-    console.log("Modo mobile ativado");
- 
-  }}
+}
 
 // Verifica o tamanho da tela quando a página é carregada
 verificarTamanhoDaTela();
 
 // Adiciona um evento de redimensionamento para verificar sempre que a tela é redimensionada
 window.addEventListener("resize", verificarTamanhoDaTela);
-
-
- 
-
-
-
-
-
-
 
 
 
