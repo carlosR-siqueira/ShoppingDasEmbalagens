f08218dab53a6db359867394281fbfbf92063c99
Carlos Bekinho <carlos.dev.job@gmail.com>
2024-03-05 23:00:33 -0300

    updade pages

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,34 +1,49 @@
-const descartaveisDB = 'https://teste-908b8-default-rtdb.firebaseio.com/produto/Descartáveis.json';
-fetch(descartaveisDB, {
-    method: 'GET',
-    mode: 'cors', // Define o modo CORS
-    headers: {
-        'Content-Type': 'application/json'
-    }
-})
 
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error('Erro ao buscar dados adicionais');
+document.addEventListener("DOMContentLoaded", function () {
+    const urlParams = new URLSearchParams(window.location.search);
+    const categoria = urlParams.get('categoria');
+
+    console.log(categoria);
+    fetchProdutos(categoria);
+});
+    function fetchProdutos(categoria) {
+
+    
+
+    const descartaveisDB = `https://teste-908b8-default-rtdb.firebaseio.com/produto/${categoria}.json`;
+    
+    fetch(descartaveisDB, {
+        method: 'GET',
+        mode: 'cors', // Define o modo CORS
+        headers: {
+            'Content-Type': 'application/json'
         }
-        return response.json();
     })
-    .then((data) => {
 
-
-
-
-        for (const key in data) {
-            if (data.hasOwnProperty(key)) {
-                const produto = data[key];
-
-                prodCard = `<div class="card" category="${produto.categoria}" subCategory="${produto.subCategoria}"><div name="button" class="badge-container"></div><img src="${produto.imagemUrl}"><h2>${produto.nome}</h2><p>${produto.descricao}</p></div>`
-
-
-                document.querySelector('.containerDesc').insertAdjacentHTML('beforeend', prodCard);
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Erro ao buscar dados adicionais');
             }
-        }
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+            return response.json();
+        })
+        .then((data) => {
+
+
+            containerDesc = document.querySelector('.containerDesc').innerHTML = `<h2 class="titulo-destaque-produtos ">${categoria}</h2>`
+
+
+            for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                    const produto = data[key];
+                    prodCard = `<div class="card" category="${produto.categoria}" subCategory="${produto.subCategoria}"><div name="button" class="badge-container"></div><img src="${produto.imagemUrl}"><h2>${produto.nome}</h2><p>${produto.descricao}</p></div>`
+
+
+
+                    document.querySelector('.containerDesc').insertAdjacentHTML('beforeend', prodCard);
+                }
+            }
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
